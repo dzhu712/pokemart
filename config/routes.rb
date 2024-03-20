6d1c708c27293c1e786762872e92b00d07f490f9
Daniel Zhu <139385352+dzhu712@users.noreply.github.com>
2024-03-20 17:43:06 -0500

    Add Show Types
    Bootstrap dropdown buttons do not work.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pokemon_cards#index'
   resources :pokemon_cards, only: [:index, :show]
+  resources :types, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
