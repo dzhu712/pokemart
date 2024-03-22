e642b612a3ee2ce89bef944158bb140766ad9082
Daniel Zhu <139385352+dzhu712@users.noreply.github.com>
2024-03-22 16:55:17 -0500

    Add Pokemon Cards Admin

--- app/models/type.rb
+++ app/models/type.rb
@@ -2,4 +2,8 @@ class Type < ApplicationRecord
   has_and_belongs_to_many :pokemon_cards
 
   validates :name, presence: true, uniqueness: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "id_value", "name", "updated_at"]
+  end
 end
