ebef37a6cabcca3e1553e4452993998358dc629a
Daniel Zhu <139385352+dzhu712@users.noreply.github.com>
2024-03-22 17:01:00 -0500

    Add Types Admin

--- app/models/type.rb
+++ app/models/type.rb
@@ -3,6 +3,10 @@ class Type < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
 
+  def self.ransackable_associations(auth_object = nil)
+    ["pokemon_cards"]
+  end
+
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "id", "id_value", "name", "updated_at"]
   end
