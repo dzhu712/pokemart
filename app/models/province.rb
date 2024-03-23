eb23df904449d749be146a06902351bfd1ee1c07
Daniel Zhu <139385352+dzhu712@users.noreply.github.com>
2024-03-23 15:52:21 -0500

    Add Provinces Admin Dashboard

--- app/models/province.rb
+++ app/models/province.rb
@@ -2,4 +2,12 @@ class Province < ApplicationRecord
   has_many :tax_histories
 
   validates :name, presence: true
+
+  def self.ransackable_associations(auth_object = nil)
+    ["tax_histories"]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "id_value", "name", "updated_at"]
+  end
 end
