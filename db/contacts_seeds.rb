1275d1dc9b30a17b115231d2d514dd1fee0c2e66
Daniel Zhu <139385352+dzhu712@users.noreply.github.com>
2024-03-22 20:45:44 -0500

    Separate Seeds

---
+++ db/contacts_seeds.rb
@@ -0,0 +1,19 @@
+puts 'Add Contact Page contents.'
+
+Contact.create(
+  title: "Daniel Zhu",
+  email: "daniel@example.com",
+  phone: "111-222-3333"
+)
+Contact.create(
+  title: "Elon Musk",
+  email: "elon@example.com",
+  phone: "444-555-6666"
+)
+Contact.create(
+  title: "Bill Gates",
+  email: "bill@example.com",
+  phone: "777-888-9999"
+)
+
+puts 'Contact Page contents added.'
