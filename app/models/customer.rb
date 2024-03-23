989a2b0a9919f61385df951c088ad08557b8cbcf
Daniel Zhu <139385352+dzhu712@users.noreply.github.com>
2024-03-23 16:32:40 -0500

    Add Customer Model

---
+++ app/models/customer.rb
@@ -0,0 +1,11 @@
+class Customer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  belongs_to :province
+
+  validates :username, presence: true
+  validates :address, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_23_202235) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_23_212710) do
   create_table "abouts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -80,6 +80,22 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_23_202235) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "address"
+    t.integer "province_id"
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["province_id"], name: "index_customers_on_province_id"
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
   create_table "pokemon_cards", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -118,5 +134,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_23_202235) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "customers", "provinces"
   add_foreign_key "tax_histories", "provinces"
 end
