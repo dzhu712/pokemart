69b7f60ea92948dc69a81d3a31665907be4dd859
Daniel Zhu <139385352+dzhu712@users.noreply.github.com>
2024-03-23 15:52:37 -0500

    Add Tax Histories Admin Dashboard

--- app/models/tax_history.rb
+++ app/models/tax_history.rb
@@ -5,6 +5,14 @@ class TaxHistory < ApplicationRecord
   validates :pst, :gst, :hst, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
   validate :at_least_one_tax_present_and_positive
 
+  def self.ransackable_associations(auth_object = nil)
+    ["province"]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "gst", "hst", "id", "id_value", "province_id", "pst", "updated_at"]
+  end
+
   private
 
   def at_least_one_tax_present_and_positive
