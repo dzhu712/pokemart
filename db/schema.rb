35f259ddc73e83b1b0e12cedef227648e4432657
Daniel Zhu <139385352+dzhu712@users.noreply.github.com>
2024-03-23 15:26:50 -0500

    Populate TaxHistories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_23_002154) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_23_202235) do
   create_table "abouts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -94,6 +94,22 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_23_002154) do
     t.integer "type_id", null: false
   end
 
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tax_histories", force: :cascade do |t|
+    t.decimal "pst"
+    t.decimal "gst"
+    t.decimal "hst"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "province_id", null: false
+    t.index ["province_id"], name: "index_tax_histories_on_province_id"
+  end
+
   create_table "types", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -102,4 +118,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_23_002154) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "tax_histories", "provinces"
 end
