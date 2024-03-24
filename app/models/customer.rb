9578833d8a94539afe491977f5d28421c31bf8cd
Daniel Zhu <139385352+dzhu712@users.noreply.github.com>
2024-03-23 22:49:58 -0500

    Allow Province to be Null

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,8 +4,8 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :province
+  belongs_to :province, optional: true
 
-  validates :username, presence: true, format: { with: /\A[a-zA-Z0-9_-]{3,15}\z/, message: "should be 3 to 15 characters long and can only contain letters, digits, underscores, and hyphens" }
+  validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9_-]{3,15}\z/, message: "should be 3 to 15 characters long and can only contain letters, digits, underscores, and hyphens" }
   validates :email, format: { with: /\A\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+\z/, message: "is invalid" }
 end
